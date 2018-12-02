3030b8d8b0c5b9d420e3904a855204e291130661
kajetan.targonski <kajetan.targonski@gmail.com>
2018-12-02 20:12:11 +0100

    AOC01 - PART 2

--- AOC01/app01.py
+++ AOC01/app01.py
@@ -13,10 +13,35 @@ def solve1(input):
 
     return result
 
+
+def solve2(input):
+    # input is circular - may be needed to reiterate over and over again
+    length = len(input)
+
+    occurences = set()
+    occurences.add(0)
+
+    ix = -1
+    curr_value = 0
+
+    while True:
+
+        ix = 0 if ix == length - 1 else ix + 1
+
+        curr_value += int(input[ix])
+
+        if curr_value in occurences:
+            return curr_value
+        else:
+            occurences.add(curr_value)
+
+
 if __name__ == "__main__":
 
-    input1 = get_input('D:\\repos\\adventofcode2018\\AOC01\\in01.io')
+    input = get_input('D:\\repos\\adventofcode2018\\AOC01\\in01.io')
 
-    result = solve1(input1)
+    result1 = solve1(input) #result1 = 425
+    print(f'The result is: {result1}')
 
-    print(f'The result is: {result}')
+    result2 = solve2(input) # result 2 => 57538
+    print(f'The result is: {result2}')
