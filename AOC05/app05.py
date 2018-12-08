392b16908a5abbde0ac92d87fce996feda52913d
kajetan.targonski <kajetan.targonski@gmail.com>
2018-12-08 20:28:21 +0100

    AOC05 - part 2

--- AOC05/app05.py
+++ AOC05/app05.py
@@ -1,4 +1,5 @@
-from itertools import product, chain
+from itertools import chain
+import operator
 
 def get_input(filename):
     with open(filename) as f:
@@ -8,13 +9,13 @@ def get_input(filename):
 def react_polymer(polymer):
 
     def should_destroy(a, b):
-        return a != b and a.lower() == b.lower()
+        return a != b and (a.lower() == b or a.upper() == b)
 
     i = 0
 
     while i < len(polymer) - 1:
 
-        if should_destroy(polymer[i], polymer[i+1]) == False:
+        if not should_destroy(polymer[i], polymer[i+1]):
             i += 1
             continue
 
@@ -28,17 +29,22 @@ def react_polymer(polymer):
 def solve1(polymer):
     return len(react_polymer(polymer))
 
-def solve2(input):
+def solve2(polymer):
 
-    pass
+    letters = set(map(lambda x: x.lower(), polymer))
+    results = dict(map(lambda x: (x, 0), letters))
+
+    for k in letters:
+        results[k] = len(react_polymer(list(filter(lambda x: x != k and x != k.upper(), polymer))))
+
+    return min(results.items(), key=operator.itemgetter(1))
 
 
 if __name__ == "__main__":
     input = get_input('D:\\repos\\adventofcode2018\\AOC05\\input.io')
-    #input = ['dabAcCaCBAcCcaDA']
 
     result1 = solve1(list(input[0])) #result1 => 10250
     print(f'The result is: {result1}')
 
-    result2 = solve2(list(input[0])) # result 2 =>
+    result2 = solve2(list(input[0])) # result 2 => 6188
     print(f'The result is: {result2}')
